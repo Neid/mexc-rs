17d031b231b1919cae6404effbdb99bc5831cfac
piotr <sofre@protonmail.com>
2025-09-12 18:54:28 +0200

    Protobuf websocket channels

--- src/spot/ws/topic.rs
+++ src/spot/ws/topic.rs
@@ -28,20 +28,20 @@ impl Topic {
 
     pub fn to_topic_subscription_string(&self) -> String {
         match self {
-            Topic::AccountDeals => "spot@private.deals.v3.api".to_string(),
-            Topic::AccountOrders => "spot@private.orders.v3.api".to_string(),
-            Topic::AccountUpdate => "spot@private.account.v3.api".to_string(),
+            Topic::AccountDeals => "spot@private.deals.v3.api.pb".to_string(),
+            Topic::AccountOrders => "spot@private.orders.v3.api.pb".to_string(),
+            Topic::AccountUpdate => "spot@private.account.v3.api.pb".to_string(),
             Topic::Deals(deals_topic) => format!(
-                "spot@public.deals.v3.api@{symbol}",
+                "sspot@public.aggre.deals.v3.api.pb@10ms@{symbol}",
                 symbol = deals_topic.symbol
             ),
             Topic::Kline(kline_topic) => format!(
-                "spot@public.kline.v3.api@{symbol}@{interval}",
+                "spot@public.kline.v3.api.pb@{symbol}@{interval}",
                 symbol = kline_topic.symbol,
                 interval = kline_topic.interval.as_ref()
             ),
             Topic::Depth(depth_topic) => format!(
-                "spot@public.increase.depth.v3.api@{symbol}",
+                "sspot@public.aggre.depth.v3.api.pb@10@{symbol}",
                 symbol = depth_topic.symbol
             ),
             Topic::BookTicker(symbol) => format!(
